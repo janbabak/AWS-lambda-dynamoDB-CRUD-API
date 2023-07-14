874d521f108667c2396d756dee9c6eb664f07ac6
Jan Babák <babakhonza@gmail.com>
2023-07-14 13:24:09 +0200

    Add: create/update product

--- src/main/java/com/janbabak/lambda/api/ProductLambdaHandler.java
+++ src/main/java/com/janbabak/lambda/api/ProductLambdaHandler.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import com.janbabak.lambda.model.Product;
@@ -13,11 +14,17 @@ import org.json.simple.parser.ParseException;
 
 import java.io.*;
 
-// handler com.janbabak.lambda.api.ProductLambdaHandler::handleRequest
 
 public class ProductLambdaHandler implements RequestStreamHandler {
 
     private String DYNAMO_TABLE = "Products";
+    /**
+     * Get product by id (path parameter or query string parameter)
+     * @param inputStream
+     * @param outputStream
+     * @param context
+     * @throws IOException
+     */
     @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
         OutputStreamWriter writer = new OutputStreamWriter(outputStream);
@@ -69,4 +76,49 @@ public class ProductLambdaHandler implements RequestStreamHandler {
         reader.close();
         writer.close();
     }
+
+    /**
+     * Create or update product.
+     * @param inputStream
+     * @param outputStream
+     * @param context
+     * @throws IOException
+     */
+    public void handlePutRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        OutputStreamWriter writer = new OutputStreamWriter(outputStream);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        JSONParser parser = new JSONParser();
+        JSONObject responseObject = new JSONObject();
+        JSONObject responseBody = new JSONObject();
+
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.defaultClient();
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        try {
+            JSONObject reqObject = (JSONObject) parser.parse(reader);
+
+            if (reqObject.get("body") != null) {
+                Product product = new Product((String) reqObject.get("body"));
+
+                dynamoDB.getTable(DYNAMO_TABLE)
+                        .putItem(new PutItemSpec().withItem(
+                                        new Item()
+                                                .withNumber("id", product.getId())
+                                                .withString("name", product.getName())
+                                                .withNumber("price", product.getPrice())
+                                )
+                        );
+                responseBody.put("message", "New Item created/updated");
+                responseObject.put("statusCode", 200);
+                responseObject.put("body", responseBody.toString());
+            }
+        } catch (ParseException e) {
+            context.getLogger().log("ERROR: " + e.getMessage());
+        }
+
+        writer.write(responseObject.toJSONString());
+        reader.close();
+        writer.close();
+    }
+
 }
